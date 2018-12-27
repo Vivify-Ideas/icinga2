8a1494940868d6078af12a84ac10a08939e59c9e
Goran Ninković <warhawk@hotmail.rs>
2018-12-27 16:18:49 +0100

    fix syntax

--- master/get_master_ticket_salt.sh
+++ master/get_master_ticket_salt.sh
@@ -1,4 +1,3 @@
- #!/bin/sh
-docker exec -it $(docker ps -aqf "name=master_icinga2") bash -c "icinga2 object list | grep salt | grep -oP 
-'\".*\"' | cut -c 2-33"
+#!/bin/sh
+docker exec -it $(docker ps -aqf "name=master_icinga2") bash -c "icinga2 object list | grep salt | grep -oP '\".*\"' | cut -c 2-33"
 
