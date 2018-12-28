df5ce7f9def125e40aff0816e414ff5971a84bae
Goran Ninković <warhawk@hotmail.rs>
2018-12-28 11:30:40 +0100

    prefix of icinga2 container should be icinga, since that will probably be the name of the stack

--- master/get_master_ticket_salt.sh
+++ master/get_master_ticket_salt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker exec -it $(docker ps -aqf "name=master_icinga2") bash -c "icinga2 object list | grep salt | grep -oP '\".*\"' | cut -c 2-33"
+docker exec -it $(docker ps -aqf "name=icinga2_icinga2") bash -c "icinga2 object list | grep salt | grep -oP '\".*\"' | cut -c 2-33"
 
